11cf442e9c355db82440297d7afe4af3b363fa2b
pompodar <pompodar@gmail.com>
2022-02-17 12:16:26 +0200

    states

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
 import TodoList from './TodoList';
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef();
+
+  function handleAddTodo (e) {
+    const name = todoNameRef.current.value;
+    if (name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: 1, name: name, complete: false}]
+    });    
+    todoNameRef.current.value = null;
+  }
   return (
-    <TodoList />
+    <>
+        <TodoList todos={ todos }/>
+        {/* { todos } */}
+        <input ref={ todoNameRef } type='text'></input>
+        <button onClick={ handleAddTodo }>Add</button>
+        <button>Clear</button>
+        <div>0 left to do</div>
+    </>
   );
 }
 
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import Todo from './Todo';
 
-export default function TodoList() {
+export default function TodoList({ todos }) {
   return (
-    <div>TodoList</div>
+    <div>
+        { todos.map(todo => {
+            return <Todo key={ todo.id } todo={ todo.name } />;
+        }) }
+    </div>
   )
 }
