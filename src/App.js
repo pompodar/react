a75ff2ef283a767e30dfc5edc13ff8da07ea7de4
pompodar <pompodar@gmail.com>
2022-02-22 10:07:42 +0200

    JSinHTML

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+const name = 'Svjat';
+const surName = 'K.';
+
+let x = false;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { 1 + 1 }
+      {x ? name : surName  }
     </div>
   );
 }
