694c0fa82a66e2b595e73153e21b2001121cb93d
pompodar <pompodar@gmail.com>
2022-02-23 09:49:10 +0200

    classesAndFunctionsComponents

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 const name = 'Svjat';
 const surName = 'K.';
 
@@ -7,8 +9,10 @@ let x = false;
 function App() {
   return (
     <div className="App">
+      <Header />
       { 1 + 1 }
       {x ? name : surName  }
+      <Footer/>
     </div>
   );
 }
